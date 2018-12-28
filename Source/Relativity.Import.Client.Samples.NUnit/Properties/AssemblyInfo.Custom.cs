ea8204d8fc28589588c1908c954dac6c0393f0a8
Scott Parillo <scott.parillo@relativity.com>
2018-12-28 06:55:04 -0800

    Build Scripts and Source Layout - Psake-based PS scripts added to support build/test via PS scripts using the latest packages. - Moved source under the Source folder to be aligned with template repos.

---
+++ Source/Relativity.Import.Client.Samples.NUnit/Properties/AssemblyInfo.Custom.cs
@@ -0,0 +1,12 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Relativity ODA LLC">
+//   © Relativity All Rights Reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Relativity.Import.Client.Samples.NUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Relativity.Import.Client.Samples.NUnit")]
